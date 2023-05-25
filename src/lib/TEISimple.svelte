4cb52f8c36c9aa2cdb054165d4730194e71d1e74
tiagosousagarcia <tiagosousagarcia@gmail.com>
2023-05-25 14:43:01 +0100

    added test of behaviours, need to abstract it to another file

--- src/lib/TEISimple.svelte
+++ src/lib/TEISimple.svelte
@@ -11,6 +11,33 @@
 
     onMount(async () => {
         var cetei = new CETEI();
+        cetei.addBehaviors({ "tei": {
+            "note": function(elt){
+                if (!this.noteIndex){
+                    this["noteIndex"] = 1;
+                } else {
+                    this.noteIndex++;
+                }
+                let id = "note" + this.noteIndex;
+                let link = document.createElement("a");
+                link.setAttribute("id", "src" + id);
+                link.setAttribute("href", "#" + id);
+                link.innerHTML = this.noteIndex;
+                let content = document.createElement("sup");
+                content.appendChild(link);
+                let notes = this.dom.querySelector("ol.notes");
+                if (!notes) {
+                    notes = document.createElement("ol");
+                    notes.setAttribute("class", "notes");
+                    this.dom.appendChild(notes);
+                }
+                let note = document.createElement("li");
+                note.id = id;
+                note.innerHTML = "<a href=\"#src" + id + "\">^</a> " + elt.innerHTML
+                notes.appendChild(note);
+                return content;
+                }
+        }});
         cetei.getHTML5(path, function(data) {
             document.getElementById('TEI-container').appendChild(data);
         });
