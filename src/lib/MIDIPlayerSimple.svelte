5afabd3ff486dd0edaf572face33b0ca682030bf
Tiago Sousa Garcia <tiagosousagarcia@gmail.com>
2023-06-15 12:01:19 +0100

    midiplayer works but is flaky and deprecated -- need to find a better solution

--- src/lib/MIDIPlayerSimple.svelte
+++ src/lib/MIDIPlayerSimple.svelte
@@ -9,17 +9,52 @@
 -->
 
 <script>
+    import Icon from '@iconify/svelte';
+    import { onMount } from 'svelte';
+    import { RangeSlider } from '@skeletonlabs/skeleton';
     export let midiFile = undefined;
 
+    let playEvent = undefined;
+    let pause = false;
+    let totalTime = 0;
+    let currentTime = 0;
+
     const startPlay = function() {
-        console.log('start');
-        MIDIjs.play(midiFile);
+        console.log(playEvent);
+        if (!playEvent) {
+            MIDIjs.play(midiFile);
+        } else if (pause) {
+            MIDIjs.resume();
+            pause = false;
+        } else if (playEvent.status === 'playing') {
+            MIDIjs.pause();
+            pause = true;
+        }
     }
 
     const stopPlay = function() {
-        console.log('stop');
         MIDIjs.stop();
+        playEvent = undefined;
+        currentTime = 0;
     }
+    
+    const playerEvents = function (event) {
+        playEvent = event;
+        currentTime = playEvent.time;
+    }
+
+    function secsToMinSecs(time) {
+        let minutes = Math.floor(time / 60);
+        let seconds = Math.floor(time - minutes *60);
+        minutes = minutes.toString().padStart(2, '0');
+        seconds = seconds.toString().padStart(2, '0');
+        return `${minutes}:${seconds}`
+    }
+
+    onMount( () => {
+        MIDIjs.player_callback = playerEvents;
+        MIDIjs.get_duration(midiFile, (seconds) => {totalTime = seconds});
+    });
 
 </script>
 
@@ -27,5 +62,10 @@
     <script src="https://www.midijs.net/lib/midi.js"></script>
 </svelte:head>
 
-<button id="playMIDI" on:click={startPlay}>Play</button>
-<button id="stopMIDI" on:click={stopPlay}>Stop</button>
+<button id="playMIDI" on:click={startPlay}><Icon icon="material-symbols:play-pause"/></button>
+<button id="stopMIDI" on:click={stopPlay}><Icon icon="material-symbols:stop"/></button>
+<span>
+    {secsToMinSecs(currentTime)}
+    <RangeSlider bind:value={currentTime} bind:max={totalTime} step={0.2}/>
+    {secsToMinSecs(totalTime)}
+</span>
