f65d47e7d764844b07b270b3e0713a2cfed970f6
tiagosousagarcia <tiagosousagarcia@gmail.com>
2023-11-07 10:14:14 +0000

    clean up build, added latest changes

--- build/_app/immutable/chunks/viewChoice.2191b11d.js
+++
@@ -1,1 +0,0 @@
-import{l as e}from"./ProgressBar.svelte_svelte_type_style_lang.84e8538a.js";const r=e("view","literature");export{r as v};
--- build/content/literature/00_intro.md
+++
@@ -1,7 +0,0 @@
----
-title: Intro
-type: md
-order: 00
----
-
-This is a _test_ markdown in **literature**!
