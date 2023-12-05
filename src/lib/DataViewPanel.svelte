1495fefcf09676eae63b4fc74f913faaabe1d265
Tiago Sousa Garcia <tiagosousagarcia@gmail.com>
2023-12-05 12:16:01 +0000

    created basic dropdown group selector

--- src/lib/DataViewPanel.svelte
+++ src/lib/DataViewPanel.svelte
@@ -1,8 +1,20 @@
 <script>
     import RawDataTable from "./RawDataTable.svelte";
+    import GroupSelector from "./GroupSelector.svelte";
     export let datasets
+
+    let selected = 'All'
+
+    function getGroups(group, dataset) {
+        // group is the column that contains the selector
+        let result = new Set(dataset.map(entry => entry[group]))
+        return ['All', ...result]
+    }
+
+    $: console.log(selected);
 </script>
 
 {#each datasets as entry}
+    <GroupSelector groups={getGroups('Treatment group', entry.data)} name = {'Treatment group'} bind:selected/>
     <RawDataTable tableObject={entry}/>
 {/each}
---
+++ src/lib/GroupSelector.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let groups;
+    export let selected;
+    export let name
+    
+</script>
+
+<label for={name}>{name}</label>
+<select id={name} bind:value={selected}>
+    {#each groups as group}
+        <option value={group}>{group}</option>
+    {/each}
+</select>
