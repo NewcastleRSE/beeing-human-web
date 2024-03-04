c24fdaff3df170078240bc8e5c816ec155bbb5eb
tiagosousagarcia <tiagosousagarcia@gmail.com>
2024-03-04 10:16:35 +0000

    corrected link creation

--- src/lib/PortalDestinationListCard.svelte
+++ src/lib/PortalDestinationListCard.svelte
@@ -2,12 +2,15 @@
     export let section;
     export let content;
     export let link;
+    import {base} from '$app/paths'
+
+    let linkString = `${base}/content/${section}#${link}`
 </script>
 
 <div class="card">
     <header class="card-header">{section}</header>
     <section class="p-4">{content}</section>
     <footer class="card-footer">
-        <a href="/content/{section}#{link}">&#11157</a>
+        <a href={linkString}>&#11157</a>
     </footer>
 </div>
