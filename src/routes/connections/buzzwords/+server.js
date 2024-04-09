b0cde173775bb5399797d314dcd7d47261faa985
Tiago Sousa Garcia <tiagosousagarcia@gmail.com>
2024-04-09 10:42:48 +0100

    successfully parses buzzwords with portals and returns an object through the api

--- src/routes/connections/buzzwords/+server.js
+++ src/routes/connections/buzzwords/+server.js
@@ -1,42 +1,42 @@
 import {json} from '@sveltejs/kit'
 import { getFileNameFromPathWithoutExtension } from '../../../utils/stringOperations.js';
 import parseMD from 'parse-md'
+import * as cheerio from 'cheerio'
+
 export const prerender = true
 
 export async function GET(event) {
-    const buzzwords = getBuzzwordsAPI();
-    return json(buzzwords)
+    const portalBuzzwords = getBuzzwordsAPI();
+    return json(portalBuzzwords);
 }
 
 function getBuzzwordsAPI() {
     let listBuzzWords = import.meta.glob('./../buzzwords/*.md', {as: 'raw', eager: true});
-    let buzzwords = {}
+    let portalBuzzwords = {}
     for (const buzz in listBuzzWords) {
         let path = JSON.stringify(buzz);
         let id = getFileNameFromPathWithoutExtension(path);
         const {_, content} = parseMD(listBuzzWords[buzz]);
-        
-        // REPLACE REGEX WITH A DOMPARSER 
-        const regexp = /<Portal(.*)>(.*)<\/Portal>/g
-        
-        const tags = content.matchAll(regexp)
-        const listTags = []
-        for (const tag of tags) {
-            console.log(tag)
-            if (tag[0]) {
-                listTags.push(tag[0]);
-            }
-        }
 
-        // THIS DOES NOT WORK ON SERVER, USE JSDOM or ANYTHING CAPABLE OF PARSING XML-LIKE ELEMENTS
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(content);
-        console.log(doc);
-        if (listTags != []) {
-            buzzwords[id] = listTags;
+        // using cheerio to parse and navigate any xml-like content inside the buzzword (i.e., like a <Portal> component. Wrapping content in a <root> to facilitate this. The $variable notation is the convention in cheerio, which is based on jquery);
+
+        // loads content as a cheerio object
+        const $doc = cheerio.load(`<root>${content}</root>`);
+
+        // gets any Portal elements
+        const $portals = $doc('Portal')
+
+        // If Portals exist, iterate through them 
+        if  ($portals.length > 0) {
+            for (const $portal of $portals) {
+                const portalId = $doc($portal).attr('id');
+                const portalContent = $doc($portal).text();
+                // adds portal to the response object
+                portalBuzzwords[portalId] = {content: portalContent}
+            }
         }
     }
 
-    return buzzwords
+    return portalBuzzwords
 }
 
