151ef860f6edcbaeede4a65cd6761df8979955c0
Tiago Sousa Garcia <tiagosousagarcia@gmail.com>
2024-05-01 11:53:28 +0100

    finished E2E testing

--- tests/E2E/Portals.test.js
+++ tests/E2E/Portals.test.js
@@ -426,13 +426,94 @@ for (const p of pages) {
     
                     for (const card of cards) {
                         const cardContent = await card.getByTestId('card-section').textContent();
+                        // expects card  to have a link
                         await expect(card.getByRole('link')).toBeAttached();
+
+                        // gets id for destination portal, builds a regex to find the data-testid in the page it navigates to
                         const expectedLink = await card.getByRole('link').getAttribute('href');
+                        const destinationId = expectedLink.split('#')[1]
+                        const destinationIdRegEx = new RegExp(`\\w+-portal-${destinationId}`)
+
+                        // navigate to portal
                         await card.getByRole('link').click();
+
+                        // page should have the correct url
                         await expect(page).toHaveURL(expectedLink);
-                        // Finding context by text will def fail in dev because text repeats itself a lot -- instead of finding by text, first find by test-id (with the dest id expected) then find the text in that element;
-                        await expect(page.getByText(cardContent)).toBeVisible();
-                        await expect(page.getByText(cardContent)).toBeInViewport();
+                        
+                        // portal should be visible and in viewport
+                        await expect(page.getByTestId(destinationIdRegEx)).toBeVisible();
+                        await expect(page.getByTestId(destinationIdRegEx)).toBeInViewport();
+
+                        // go back to the previous page and if necessary opens the portal again
+                        await page.goBack();
+                        await expect(page).toHaveURL(`/${p}`);
+                        if (await card.isVisible() === false){
+                            await portal.click();
+                        }
+                    }
+    
+                    // click close button
+                    const closeButton = panel.getByRole('button');
+                    closeButton.click();
+    
+                    await expect(panel).not.toBeInViewport();
+                }
+            }
+        });
+
+        test(`All portals' links followed should be highlighted -- ${p}`, async ({page}) => {
+            await expect(page).toHaveURL(`/${p}`);
+    
+            // finds all portals
+            const portals = await page.locator('.portal').all();
+            const portalIds = []
+            for (const portal of portals) {
+                portalIds.push(await portal.getAttribute('data-testid'));
+            }
+    
+            // filters portals to only clickable ones
+            // iterates through portal ids rather than portals to avoid errors if the portal order changes after page navigation
+            for (const portalID of portalIds) {
+                const portal = page.getByTestId(portalID);
+                const type = portalID.split('-')[0]
+                if (type === 'origin' || type === 'both') {
+                    // clicks the portal
+                    await portal.click();
+    
+                    // finds the panel
+                    const panel = page.getByRole('dialog');
+    
+                    // .toBeVisible() fails the test despite the panel being visible in the preview
+                    await expect(panel).toBeInViewport()
+    
+                    // get card collection
+                    const cards = await panel.getByTestId('portal-panel-card').all();
+                    expect(cards.length).toBeGreaterThan(0);
+    
+                    for (const card of cards) {
+                        const cardContent = await card.getByTestId('card-section').textContent();
+                        // expects card  to have a link
+                        await expect(card.getByRole('link')).toBeAttached();
+
+                        // gets id for destination portal, builds a regex to find the data-testid in the page it navigates to
+                        const expectedLink = await card.getByRole('link').getAttribute('href');
+                        const destinationId = expectedLink.split('#')[1]
+                        const destinationIdRegEx = new RegExp(`\\w+-portal-${destinationId}`)
+
+                        // navigate to portal
+                        await card.getByRole('link').click();
+
+                        // page should have the correct url
+                        await expect(page).toHaveURL(expectedLink);
+                        
+                        // portal should be visible and in viewport
+                        await expect(page.getByTestId(destinationIdRegEx)).toBeVisible();
+                        await expect(page.getByTestId(destinationIdRegEx)).toBeInViewport();
+
+                        // portal should contain 'underline' class
+                        await expect(page.getByTestId(destinationIdRegEx)).toHaveClass(/underline/);
+
+                        // go back to the previous page and if necessary opens the portal again
                         await page.goBack();
                         await expect(page).toHaveURL(`/${p}`);
                         if (await card.isVisible() === false){
