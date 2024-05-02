80a067991d15f9d7404e022b6b7eecaafc5d7e98
tiagosousagarcia <tiagosousagarcia@gmail.com>
2024-05-02 12:01:34 +0100

    added a couple of debugging console logs

--- src/lib/InjectBuzzword.svelte
+++ src/lib/InjectBuzzword.svelte
@@ -13,7 +13,7 @@
   -->
 
 <script>
-  import { onMount } from "svelte";
+  import { onDestroy, onMount } from "svelte";
 
   export let buzzName = undefined;
   let buzzword = undefined;
@@ -34,7 +34,12 @@
         errorCode = 2
       }
     }
+    console.log(`Mounting buzz with id ${buzzName}`)
   });
+
+  onDestroy(async () => {
+    console.log(`Destroying buzz with id ${buzzName}`);
+  })
 </script>
 
 {#if buzzword != undefined && errorCode == 0 }
