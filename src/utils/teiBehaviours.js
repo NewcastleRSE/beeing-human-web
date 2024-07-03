107634af34c39c51536f26925937edb1d3cf8bbc
Tiago Sousa Garcia <tiagosousagarcia@gmail.com>
2024-07-03 12:32:31 +0100

    removed extra behaviours, added correct styling for transformation

--- src/utils/teiBehaviours.js
+++ src/utils/teiBehaviours.js
@@ -32,23 +32,6 @@ export let teiBehaviours = {
                     notes.appendChild(note);
                     return content;
                 }
-            ],
-
-            ["[subtype=summary]",
-                function (elt) {
-                    const content = elt.innerText.split('. ');
-                    elt.innerText = `${content[0]}. `
-                    const itSpan = document.createElement('span');
-                    if (content.lenght <= 2) {
-                        itSpan.innerText = content[1];
-                    } else {
-                        const italics = content.slice(1);
-                        itSpan.innerText = italics.join(". ");
-
-                    }
-                    itSpan.classList.add('form-italics');
-                    elt.appendChild(itSpan);
-                }
             ]
         ],
         "ptr": function (elt) {
@@ -65,16 +48,6 @@ export let teiBehaviours = {
             if (elt.getAttribute('target') === '#') {
                 console.log('Ignoring empty refs...')
             } else {
-                let indexOfAlpha = []
-                let indexOfDigit = []
-                for (let index = 0; index < elt.innerText.lenght; index++) {
-                    if (!parseInt(elt.innerText[index])) {
-                        indexOfAlpha.push(index)
-                    } else {
-                        indexOfDigit.push(index)
-                    }
-                }
-                console.log(indexOfAlpha, indexOfDigit)
                 var link = document.createElement('a');
                 link.href = elt.getAttribute('target');
                 link.innerHTML = elt.innerHTML;
