40e3fa3282b4ccf0593b3665565b2ffe6ca12d10
tiagosousagarcia <tiagosousagarcia@gmail.com>
2024-08-06 12:01:58 +0100

    drew hexagon and border with css, need to adjust overlay

---
+++ src/lib/Cell.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<div class="cell-container">
+    <div class="hexagon flex justify-center items-center bg-black opacity-50">
+        <p class="size-fit">TEXT</p>
+    </div>
+</div>
+
+<style>
+    .hexagon {
+        position: absolute;
+        top: 4px; /*equals the size of the border*/
+        left: 4px; /*equals the size of the border*/
+        width: 138px; /*original dimensions*/
+        height: 122px; /*original dimensions*/
+        clip-path: url('hexagon-cell.svg#hexagon-cell');
+        -webkit-clip-path: url('hexagon-cell.svg#hexagon-cell');
+        background: url("https://picsum.photos/200");
+    }
+
+    .cell-container{
+        position: relative;
+        background: theme('colors.primary.600');
+        width: 146px; /*equals original dimensions plus 2*border size*/
+        height: 130px; /*equals original dimensions plus 2*border size*/
+        clip-path: url('hexagon-cell.svg#hexagon-cell');
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,14 @@
   import "../app.postcss";
 
   import LogoLandingPage from "../lib/LogoLandingPage.svelte";
+  import Cell from "../lib/Cell.svelte";
 </script>
 
-<header class="flex flex-col w-full items-center text-lg shrink">
+<header class="flex flex-col w-full items-center shrink">
   <LogoLandingPage/>
-  <p class="font-serif md:w-max">An exploration of Charles Butler’s <em>The Feminine Monarchie</em></p>
+  <p class="font-serif text-center md:w-max md:text-lg">An exploration of Charles Butler’s <em>The Feminine Monarchie</em></p>
 </header>
 
+<Cell/>
 
 <slot />
