f0982cf0cb3ff686eaa36df8ff8f625a4c25c4c9
tiagosousagarcia <tiagosousagarcia@gmail.com>
2024-08-07 12:03:39 +0100

    still need to fix css path problem, clip-path not working in chrome

--- src/lib/Cell.svelte
+++ src/lib/Cell.svelte
@@ -2,8 +2,8 @@
 </script>
 
 <div class="cell-container">
-    <div class="hexagon flex justify-center items-center bg-black opacity-50">
-        <p class="size-fit">TEXT</p>
+    <div class="hexagon flex justify-center items-center bg-black">
+        <p class="size-fit text-white">TEXT</p>
     </div>
 </div>
 
@@ -14,16 +14,26 @@
         left: 4px; /*equals the size of the border*/
         width: 138px; /*original dimensions*/
         height: 122px; /*original dimensions*/
-        clip-path: url('hexagon-cell.svg#hexagon-cell');
-        -webkit-clip-path: url('hexagon-cell.svg#hexagon-cell');
-        background: url("https://picsum.photos/200");
+        -webkit-clip-path: url('assets/hexagon-cell.svg#hexagon-cell');
+        clip-path: url('assets/hexagon-cell.svg#hexagon-cell');
+        background: linear-gradient(rgba(14,27,31,0.4), rgba(14,27,31,0.4)),
+        url("https://picsum.photos/200");
+        -webkit-transition: all 3s ease;
+        -moz-transition: all 3s ease;
+        -o-transition: all 3s ease;
+        transition: all 3s ease;
     }
 
-    .cell-container{
+    .hexagon:hover {
+        background: linear-gradient(rgba(14,27,31,0.9), rgba(14,27,31,0.9)), url("https://picsum.photos/200");
+
+    }
+
+    .cell-container {
         position: relative;
         background: theme('colors.primary.600');
         width: 146px; /*equals original dimensions plus 2*border size*/
         height: 130px; /*equals original dimensions plus 2*border size*/
-        clip-path: url('hexagon-cell.svg#hexagon-cell');
+        clip-path: url('assets/hexagon-cell.svg#hexagon-cell');
     }
 </style>
