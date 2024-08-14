42494f426d2abf79e5d3acf9df69ded78b35df56
tiagosousagarcia <tiagosousagarcia@gmail.com>
2024-08-14 11:52:12 +0100

    adjustments for f'lexibility, solved problem with transform, ajustements for responsiveness

--- src/routes/(sections)/+layout.svelte
+++ src/routes/(sections)/+layout.svelte
@@ -9,9 +9,10 @@
 </script>
 
 <header class="w-full">
-  <div class="flex items-center justify-center mb-6 md:mb-32 shrink">
-    <LogoLandingPage/>
-    <SectionSelector {section}/>
+  <div class="flex flex-col gap-1 md:gap-4 max-w-64 mx-auto md:max-w-full md:flex-row items-center justify-center my-6 md:my-20 shrink">
+    <LogoLandingPage class="max-h-8 md:max-h-28"/>
+    <div class="w-1/2 border-t md:w-0 md:self-center md:h-28 md:border-l md:border-t-0 border-surface-500 self-end justify-self-end"></div> 
+    <SectionSelector {section}/>             
   </div>
 </header>
 
