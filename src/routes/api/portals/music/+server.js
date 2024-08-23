caa1b5e1162f9393bd3123adc09cd593a4360a61
tiagosousagarcia <tiagosousagarcia@gmail.com>
2024-08-23 11:28:29 +0100

    portals api corrected, need to fix link

--- src/routes/api/portals/music/+server.js
+++ src/routes/api/portals/music/+server.js
@@ -4,7 +4,7 @@ import { getPortalsAPI } from '../../../../utils/apiHelper.js';
 export const prerender = true
 
 export async function GET(event) {
-    let listMds = import.meta.glob('./../../../music/*.md', {as: 'raw', eager: true});
+    let listMds = import.meta.glob('/src/routes/\*\/music/\*\/*.md', {as: 'raw', eager: true});
     const musicPortals = getPortalsAPI(listMds);
     return json(musicPortals);
 }
