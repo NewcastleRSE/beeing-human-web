fe6c870d61aae35263ee6fe5a51c2a4b292b444a
tiagosousagarcia <tiagosousagarcia@gmail.com>
2024-09-12 14:39:50 +0100

    basic styling of data source selector

--- src/lib/DataSelector.svelte
+++ src/lib/DataSelector.svelte
@@ -1,8 +1,17 @@
-<div class="flex flex-row w-full space-between">
-    <h1>DATA SOURCE</h1>
-    <div>
-        <button class="btn">One button</button>
-        <button class="btn">Two button</button>
-        <button class="btn">N button</button>
-    </div>
-</div>
+<script>
+    import {RadioGroup, RadioItem} from '@skeletonlabs/skeleton';
+    let radioValue = 0;
+</script>
+
+<form class="flex flex-row w-full bg-primary-400 justify-between content-center px-12 py-10">
+    <select name="data-source" id="data-source-select" class="select text-3xl bg-transparent border-none rounded-lg pr-16 max-w-fit font-pfdisplay hover:font-bold hover:cursor-pointer transition-all ease-in-out duration-200 motion-reduce:transition-none">
+        <option value="source 1">data source 1</option>
+        <option value="source 2">data source 2</option>
+        <option value="source 3">data source 3</option>
+    </select>
+    <RadioGroup class="h-fit self-center text-white" background="bg-secondary-500" active="bg-secondary-100 text-black" hover="hover:bg-secondary-400 transition-all ease-in-out duration-300 motion-reduce:transition-none">
+        <RadioItem bind:group={radioValue} name="btn1" value={0}>Option 1</RadioItem>
+        <RadioItem bind:group={radioValue} name="btn2" value={1}>Option 2</RadioItem>
+        <RadioItem bind:group={radioValue} name="btn3" value={2}>Option 3</RadioItem>
+    </RadioGroup>
+</form>
