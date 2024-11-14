cc2c5ecb3dddd7c3b3b5325157bf911e43e76889
tiagosousagarcia <tiagosousagarcia@gmail.com>
2024-11-14 11:59:59 +0000

    simplified display of pdfs

---
+++ src/lib/PdfViewer.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let url;
+    export let pageNum;
+    export let objectTitle
+
+    let width;
+    let height;
+
+</script>
+
+
+<div bind:clientWidth={width} bind:clientHeight={height} class="h-full">
+    <object data={url+'#page='+pageNum} type="application/pdf" width={width} height={height} title={objectTitle}>
+        Could not display facsimile
+    </object>
+</div>
