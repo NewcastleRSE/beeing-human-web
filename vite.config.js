703e985da5f4423eb8616775a83c1e9b88b3bc68
Tiago Sousa Garcia <tiagosousagarcia@gmail.com>
2024-11-15 11:49:27 +0000

    updated vitest config, testing-library, all working

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
+import { svelteTesting } from '@testing-library/svelte/vite'
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), svelteTesting()],
   test: {
     include: ["src/**/*.{test,spec}.{js,ts}", "tests/components/*.{test,spec}.{js,ts}", "tests/unit/*.{test,spec}.{js,ts}"],
     environment: 'jsdom',
-    alias: [
-      { find: /^svelte$/, replacement: "svelte/internal" }
-    ],
+    setupFiles: ['./vitest-setup.js'],
     hookTimeout: 25000,
   },
 });
