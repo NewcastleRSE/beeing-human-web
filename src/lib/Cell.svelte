ac5bdf26ec28c0fd2e5c2e26aed2083fadcfa8e2
tiagosousagarcia <tiagosousagarcia@gmail.com>
2024-11-18 12:59:57 +0000

    layout working as svelte5

--- src/lib/Cell.svelte
+++ src/lib/Cell.svelte
@@ -3,11 +3,13 @@
     import InternalLink from "./InternalLink.svelte";
     import { capitaliseFirstLetter } from "../utils/stringOperations";
 
-    export let type = undefined;
-    export let link = undefined;
-    export let text = undefined;
-    export let backgroundImage = undefined;
-    export let testid = undefined;
+    let {
+        type = undefined,
+        link = undefined,
+        text = undefined,
+        backgroundImage = undefined,
+        testid = undefined
+    } = $props();
 
     onMount(async () => {
         if (!type) {
