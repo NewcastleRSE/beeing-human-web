7a747058fa29c3208a85f12a60cf936b19bcf6de
tiagosousagarcia <tiagosousagarcia@gmail.com>
2025-01-13 16:05:09 +0000

    component now keeps track of current pages if any of the next or previous page controls are pressed

--- src/lib/IIIFViewer.svelte
+++ src/lib/IIIFViewer.svelte
@@ -1,7 +1,7 @@
 <script>
-
     import { onDestroy, onMount } from "svelte";
     import { browser } from "$app/environment";
+    import {elementReady} from '../utils/generalHelpers'
 
     // This needs to be imported only on the browser, otherwise it will generate an error
     // import "tify";
@@ -9,6 +9,7 @@
 
     let loaded = $state(false);
     let iiif = $state(undefined);
+    let currentPage = $state(undefined);
 
     let { manifest = undefined, startPage = undefined } = $props();
 
@@ -17,20 +18,60 @@
             container: "#facsimile-viewer",
             manifestUrl: manifest,
         });
-        removeHeader()
+        removeHeader();
+        addListenersToButtons();
     }
 
     async function removeHeader() {
         try {
             await iiif.ready.then(() => {
                 iiif.setPage([parseInt(startPage)]);
-                const iiifTitleHeader = document.getElementsByClassName('tify-header-title')[0];
+                const iiifTitleHeader =
+                    document.getElementsByClassName("tify-header-title")[0];
                 if (iiifTitleHeader) {
                     iiifTitleHeader.remove();
                 }
-            })
+            });
         } catch (e) {
-            console.warn('tify is not ready')
+            console.warn("tify is not ready");
+        }
+    }
+
+    async function addListenersToButtons() {
+        try {
+            await iiif.ready.then(() => {
+                // Add event listener to the next page button
+                elementReady(".tify-scan-page-button.-next").then(() => {
+                    const nextButton = document.querySelector(".tify-scan-page-button.-next");
+                    nextButton.addEventListener('click', () => {
+                        currentPage += 1;
+                    })
+                })
+
+                elementReady(".tify-header-button[title='Next page']").then(() => {
+                    const nextButton = document.querySelector(".tify-header-button[title='Next page'");
+                    nextButton.addEventListener('click', () => {
+                        currentPage += 1;
+                    });
+                })
+
+                elementReady(".tify-scan-page-button.-previous").then(() =>{
+                    const prevButton = document.querySelector(".tify-scan-page-button.-previous");
+                    prevButton.addEventListener('click', () => {
+                        currentPage -= 1;
+                    })
+                })
+
+                elementReady(".tify-header-button[title='Previous page']").then(() => {
+                    const prevButton = document.querySelector(".tify-header-button[title='Previous page'");
+                    prevButton.addEventListener('click', () => {
+                        currentPage -= 1;
+                    });
+                })
+
+            });
+        } catch (e) {
+            console.warn("tify is not ready", e);
         }
     }
 
@@ -40,27 +81,29 @@
                 iiif.setPage([parseInt(pageNumber)]);
             });
         } catch (e) {
-            console.warn('tify is not ready');
+            console.warn("tify is not ready");
         }
     }
 
     async function previousPage() {
         try {
             await iiif.ready.then(() => {
-                console.log(iiif.viewer)
+                currentPage -= 1;
+                changePage(currentPage);
             });
         } catch (e) {
-            console.warn('tify is not ready', e);
+            console.warn("tify is not ready", e);
         }
     }
 
     async function nextPage() {
         try {
             await iiif.ready.then(() => {
-                iiif.nextPage();
+                currentPage += 1;
+                changePage(currentPage);
             });
         } catch (e) {
-            console.warn('tify is not ready');
+            console.warn("tify is not ready");
         }
     }
 
@@ -70,10 +113,13 @@
                 // import tify and create a new instance
                 await import("tify").then(() => {
                     if (manifest && startPage) {
-                        buildIIIFY(manifest)
+                        buildIIIFY(manifest);
+                        if (currentPage === undefined) {
+                            currentPage = parseInt(startPage);
+                        }
                     }
                 });
-                loaded = true
+                loaded = true;
             } catch (e) {
                 console.error(e);
             }
@@ -86,11 +132,11 @@
             iiif.destroy();
         }
     });
-
 </script>
 
+{@debug currentPage}
 
-<button onclick={() => previousPage()}>-</button><button onclick={() => changePage(2)}>Go to Page 2</button><button onclick={() => nextPage()}>+</button>
-<div id="facsimile-viewer" class="h-full">
-</div>
-
+<button onclick={() => previousPage()}>-</button><button
+    onclick={() => changePage(2)}>Go to Page 2</button
+><button onclick={() => nextPage()}>+</button>
+<div id="facsimile-viewer" class="h-full"></div>
