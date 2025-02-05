d4cd81ba828cd0cdafe475d6ed40d6e0e8970604
tiagosousagarcia <tiagosousagarcia@gmail.com>
2025-02-05 09:25:01 +0000

    updated injectmd to apply typography classes

---
+++ src/utils/textClasses.js
@@ -0,0 +1,19 @@
+// Necessary for typographical conventions used by Skeleton.dev
+
+export const customClasses = {
+    h1: 'h1 my-6 font-light text-secondary-500',
+    h2: 'h2 mt-6 mb-2 font-thin',
+    h3: 'h3 font-thin',
+    h4: 'h4 font-thin',
+    h5: 'h5 font-thin',
+    h6: 'h6 font-thin',
+    a: 'anchor',
+    blockquote: 'blockquote',
+    pre: 'pre',
+    code: 'code',
+    kbd: 'kbd',
+    del: 'del',
+    ins: 'ins',
+    img: 'm-auto max-w-sm my-10',
+    li: 'list-item mb-2'
+}
