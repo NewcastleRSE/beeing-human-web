8479fe50710132adac77f4f9367d91b98fce70ad
tiagosousagarcia <tiagosousagarcia@gmail.com>
2025-02-20 15:35:39 +0000

    basic styling

--- src/lib/DataSelectorControls/DataNavigator.svelte
+++ src/lib/DataSelectorControls/DataNavigator.svelte
@@ -18,30 +18,28 @@
 </script>
 
 {#if options != undefined}
-    <div>
-        <button 
+    <div class="flex text-4xl md:text-5xl justify-center items-center text-gray-600 w-48">
+        <button
+        class="hover:font-bold hover:cursor-pointer hover:text-black transition-all ease-in-out duration-200 motion-reduce:transition-none" 
             onclick={() => {
                 const currentlySelectedIndex = options.listIndex.findIndex((section) => section[1] == selected);
                 selected = options.listIndex[currentlySelectedIndex - 1][1];
                 changeSelected();
             }}
-        disabled={selected == options.listIndex[0][1]}>
-            ←
-        </button>
-        <select bind:value={selected}
+        disabled={selected == options.listIndex[0][1]}>🢐</button>
+        <select bind:value={selected} class="select text-lg text-center md:text-xl bg-transparent border-none rounded-lg max-w-fit font-pfdisplay hover:font-bold hover:cursor-pointer transition-all ease-in-out duration-200 motion-reduce:transition-none text-black w-40"
         onchange= {changeSelected} id="navigator-select">
             {#each options.listIndex as section}
                 <option value={section[1]}>{section[0]}</option>
             {/each}
         </select>
         <button
+        class="hover:font-bold hover:cursor-pointer hover:text-black transition-all ease-in-out duration-200 motion-reduce:transition-none" 
             onclick={() => {
                 const currentlySelectedIndex = options.listIndex.findIndex((section) => section[1] == selected);
                 selected = options.listIndex[currentlySelectedIndex + 1][1];
                 changeSelected();
             }}
-        disabled={selected == options.listIndex[options.listIndex.length - 1][1]}>
-            →
-        </button>
+        disabled={selected == options.listIndex[options.listIndex.length - 1][1]}>🢒</button>
     </div>
 {/if}
