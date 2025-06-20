ba58db22a5fb17557db066cb7a3cf339d31fb903
tiagosousagarcia <tiagosousagarcia@gmail.com>
2025-06-20 08:53:16 +0100

    added simple analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,4 +9,9 @@
   let { children } = $props();
 </script>
 
+<svelte:head>
+  <!-- 100% privacy-first analytics -->
+  <script async src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+</svelte:head>
+
 {@render children?.()}
