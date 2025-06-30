53b6e51de293267297c9038b95826f5be21dfd7d
tiagosousagarcia <tiagosousagarcia@gmail.com>
2025-06-30 10:14:36 +0100

    added simon bio

--- src/routes/(sections)/about/people/[slug]/+page.svelte
+++ src/routes/(sections)/about/people/[slug]/+page.svelte
@@ -156,7 +156,7 @@
         <div class="flex flex-col">
             <h3 class="h3 text-4xl">{personData.name}</h3>
             {#if personData.title || personData.affiliation}
-            <span class="h4 text-gray-500 text-lg italic">{#if personData.title}{personData.title}{/if}{#if personData.affiliation} · {personData.affiliation}{/if}</span>
+            <span class="h4 text-gray-500 text-lg italic">{#if personData.title}{personData.title}{/if} {#if personData.affiliation}· {personData.affiliation}{/if}</span>
             {/if}
         </div>
         <InjectMD content={personData.bio} layout= {false} />
