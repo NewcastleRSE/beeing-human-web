ed88c14289f5aae8fb74365b66d89adb43b9743b
tiagosousagarcia <tiagosousagarcia@gmail.com>
2025-08-01 11:49:25 +0100

    adds favicon, changes title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,12 +6,22 @@
    */
 
   /** @type {Props} */
+  import {page} from "$app/stores";
+  
   let { children } = $props();
+
+  let section = $derived.by(() => {
+    const path = $page.route.id.split("/");
+    return path.length >= 3 ? path[2] : undefined;
+  });
 </script>
+
+{@debug section}
 
 <svelte:head>
   <!-- 100% privacy-first analytics -->
   <script async src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+  <title>Bee-ing human{section ? ` — ${section.charAt(0).toUpperCase() + section.toLowerCase().slice(1)}` : ''}</title>
 </svelte:head>
 
 {@render children?.()}
