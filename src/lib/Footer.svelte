145d9f45b6589b3c378d5418645800003843ec2a
tiagosousagarcia <tiagosousagarcia@gmail.com>
2025-12-23 09:02:53 +0000

    adds footer with date and version info

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,14 @@
+<script>
+    let version = __APP_VERSION__;
+    let buildDate = new Date(__BUILD_DATE__);
+
+    let dateString = $derived.by(() => {
+        return `${buildDate.getFullYear()}.${(buildDate.getMonth() + 1).toString()
+            .padStart(2, "0")}.${buildDate.getDate().toString().padStart(2, "0")}`
+    });
+</script>
+
+<footer class="w-full  mt-12 mb-6 py-6 text-center md:text-sm text-xs text-secondary-300">
+    <p>Beeing Human, v. {version}</p>
+    <p>{dateString}</p>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,8 @@
   
   let { children } = $props();
 
+  import Footer from "../lib/Footer.svelte";
+
   let section = $derived.by(() => {
     const path = $page.route.id.split("/");
     return path.length >= 3 ? path[2] : undefined;
@@ -23,4 +25,7 @@
   <title>Bee-ing human{section ? ` — ${section.charAt(0).toUpperCase() + section.toLowerCase().slice(1)}` : ''}</title>
 </svelte:head>
 
+<div class="min-h-screen flex flex-col">
 {@render children?.()}
+<Footer />
+</div>
