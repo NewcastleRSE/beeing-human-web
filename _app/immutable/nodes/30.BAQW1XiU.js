c324bd50d57722ecf882a3c7ce1ba7e93563b639
Tiago Sousa Garcia <tiagosousagarcia@gmail.com>
2026-01-09 12:26:28 +0000

    Updates

---
+++ _app/immutable/nodes/30.BAQW1XiU.js
@@ -0,0 +1,52 @@
+import"../chunks/DxQ8gTXH.js";import"../chunks/DMzixJe7.js";import{f as k,a as _,s as t,d as a,t as x,b as T,ak as o,r as n}from"../chunks/Unh_RLsm.js";import{a as s}from"../chunks/CcRZ_-_o.js";import{l as $,s as B}from"../chunks/96g4ABYT.js";import{A as I}from"../chunks/CUZrZbB0.js";import{b as r}from"../chunks/Bm8SzaHx.js";import"../chunks/DooAJvcs.js";import{B as D}from"../chunks/AS93wGb6.js";try{let e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},i=new e.Error().stack;i&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[i]="2e88dc4d-7564-43f8-8b7c-b7f5ea60afe3",e._sentryDebugIdIdentifier="sentry-dbid-2e88dc4d-7564-43f8-8b7c-b7f5ea60afe3")}catch{}const p={title:"Digital Thoughts",id:"digital-thoughts",parent:"literature",link:"literature/digital-thoughts",lead:"Olivia introduces the paradigm-shift — and the new investigative possibilities -- of encoding and exploring an early modern text like Butler's *Feminine Monarchie* using digital methods.",img:"assets/articles/digital-thoughts/markus-spiske-iar-afB0QQw-unsplash.jpg",imgAlt:"A matrix-style green text on black background",type:"article",layout:"article",author:"olivia",date:"2026/01/09",order:3,readingTime:{text:"4 min read",minutes:3.055,time:183300,words:611}},{title:A,id:G,parent:J,link:K,lead:U,img:V,imgAlt:X,type:F,layout:Y,author:E,date:M,order:Z,readingTime:O}=p;var j=k(`<!> <p>From day one of the project we began categorising and recording the
+details of the book in meta-languages that could translate it into
+digital data: the data you will now find in our <a href="https://github.com/NewcastleRSE/beeing-human-tei-data" rel="nofollow" class="anchor">github repository</a>. On
+our website, this is displayed as an <a class="anchor">annotated transcription alongside a
+facsimile</a>, in a form that seems familiar, though modernised, to readers
+of early modern texts. However, we (or indeed anyone; it is open access)
+could now display this data into any form they wished while still
+retaining a built-in accuracy. In this way we have not only
+futureproofed Butler's book, we have given it the ability to shapeshift
+while retaining its original essence.</p> <p>This is a profoundly Butlerian move, as he himself was deeply drawn to
+moments of translation between different forms — of insect bodies and
+hive status in <em>The Feminine Monarchie</em>, but also in his phonetic
+translation of the text, which seeks to transpose the meaning of his
+sentences into a different convention, a modulatory procedure that also
+underpins his interests in music and rhetoric. Our work restores a
+living glow to <em>The Feminine Monarchie</em> in the sense that we have made
+this important book accessible and newly useful, and <a class="anchor">the born-digital
+nature of the work</a> shows a faithfulness to the morphological
+intellectual spirit of the original, too.</p> <p>Editing any book involves immersion in close detail, and it has been
+extraordinary to spend my working days listening to the buzz amidst this
+text's words and punctuation. The digital languages we've used to make
+the book have provided yet more ways of paying close attention to the
+text.</p> <p>We have shown how Butler edits his text between the editions of 1609 and
+1623, and you will see different types of changes highlighted, with 1609
+pushing up through our 1623 copy text, or ghosting out where there have
+been deletions. These changes show Butler's thoughts as he revisits his
+original work. They show his experiences in the interim, his updated
+aims for the volume, new developments in his expression.</p> <p>By coding-in Butler's marginal references we can show the pressure
+points where his text is tightened against external sources of
+authority. Likewise, <a class="anchor">we've made visible Butler's internal
+cross-referencing system</a>, where he leads you to other places in his own
+book, creating a lattice of affinities. Our digital apparatus highlight
+the pattern in which Butler structures the knowledge in his text,
+showing us places where he increases epistemological tension to convey
+fact and certainty, or lets it sit a little more loosely in the case of
+anecdote and conjecture.</p> <p>We've used <a href="https://tei-c.org/" rel="nofollow" class="anchor">TEI</a> to reveal the discursive attention of the editorial
+team, too. Throughout the book you will find translations, but
+especially in <a class="anchor">chapter five</a> (the central chapter in which the book's
+multi-sensory imaginary crescendos around the song of the queen and her
+swarming princes) you will see we have made visible our scholarly focus
+on the text with a set of annotations. Sometimes, when you go and order
+an early modern book in the rare materials room of the library, you find
+it multiply annotated by its various owners over the years, showing an
+accretion of thoughts and clarifications. These handwritten annotations
+stop, though, as we are quite rightly not permitted to write in old,
+special books, so Butler's text had not yet heard of today's world of
+bees, music, or language. We have begun this process anew with our
+edition, and now many future readers will be able to add annotations and
+interpretations to <em>The Feminine Monarchie</em>, in forms we may not yet
+even know about, and in a natural landscape that sometimes looks
+uncertain for nature and pollinating insects — but this text is once
+again in the conversation.</p>`,1);function z(e,i){const m=$(i,["children","$$slots","$$events","$$legacy"]);I(e,B(()=>m,()=>p,{children:(f,L)=>{var u=j(),c=_(u);D(c,{get author(){return E},get date(){return M},get type(){return F},get title(){return A},get readingTime(){return O}});var h=t(c,2),w=t(a(h),3);o(),n(h);var l=t(h,2),y=t(a(l),5);o(),n(l);var d=t(l,6),b=t(a(d));o(),n(d);var g=t(d,2),v=t(a(g),3);o(3),n(g),x(()=>{s(w,"href",`${r??""}/literature/transcription`),s(y,"href",`${r??""}/literature/born-digital-edition`),s(b,"href",`${r??""}/literature/cross-references`),s(v,"href",`${r??""}/literature/transcription#ch5`)}),T(f,u)},$$slots:{default:!0}}))}function ee(e){z(e,{})}export{ee as component};
