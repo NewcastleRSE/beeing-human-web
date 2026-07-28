f0b4db8e034ce7eaa19f8bd163980055376d5736
Tiago Sousa Garcia <tiagosousagarcia@gmail.com>
2026-07-28 12:09:26 +0100

    adds umami script

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,6 +27,8 @@
 <svelte:head>
   <!-- 100% privacy-first analytics -->
   <script async src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+
+  <script defer src="https://umami-app.blueglacier-edae29ed.uksouth.azurecontainerapps.io/script.js" data-website-id="567a68a3-7ac7-4a87-8feb-d3ae863b3f1d"></script>
   <title>Bee-ing human{section ? ` — ${section.charAt(0).toUpperCase() + section.toLowerCase().slice(1)}` : ''}</title>
 </svelte:head>
 
